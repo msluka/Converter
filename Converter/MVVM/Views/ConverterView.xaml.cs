dced5afbd7a8a44ee5bfaf5c6e2b5267d717b614
Msluka <msluka.info@gmail.com>
2023-01-29 00:58:36 +0100

    Creating methods to get ObservableCollection from UnistNet NuGet Package and Binding them to Picker

--- Converter/MVVM/Views/ConverterView.xaml.cs
+++ Converter/MVVM/Views/ConverterView.xaml.cs
@@ -1,3 +1,5 @@
+using Converter.MVVM.ViewModels;
+
 namespace Converter.MVVM.Views;
 
 public partial class ConverterView : ContentPage
@@ -5,5 +7,6 @@ public partial class ConverterView : ContentPage
 	public ConverterView()
 	{
 		InitializeComponent();
+		BindingContext = new ConverterViewModel();
 	}
 }
